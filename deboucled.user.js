f1b65e3967e1263eb41d4b9a54c3dd2b9a947ef5
Rand0max <rand0max@protonmail.com>
2021-09-18 17:30:34 +0200

    Decrease blacklist button visibility + add title

--- deboucled.user.js
+++ deboucled.user.js
@@ -218,7 +218,7 @@ async function getPageContent(page) {
 }
 
 function addIgnoreButtons() {
-    const forbiddenSvg = '<svg style="display: none;"><symbol id="forbiddenlogo"><g><ellipse opacity="0.7" stroke-width="20" stroke="red" ry="70" rx="70" cy="80" cx="80" fill="none" /><line opacity="0.7" stroke="red" y2="37.39011" x2="122.60989" y1="122.60989" x1="37.39011" stroke-width="20" fill="none" /></g></symbol></svg>';
+    const forbiddenSvg = '<svg style="display: none;"><symbol id="forbiddenlogo"><g><ellipse opacity="0.6" stroke-width="20" stroke="red" ry="70" rx="70" cy="80" cx="80" fill="none" /><line opacity="0.6" stroke="red" y2="37.39011" x2="122.60989" y1="122.60989" x1="37.39011" stroke-width="20" fill="none" /></g></symbol></svg>';
     document.querySelector('.topic-list.topic-list-admin').innerHTML += forbiddenSvg;
 
     let topics = getAllTopics(document);
@@ -236,6 +236,7 @@ function addIgnoreButtons() {
         let topicSubject = topic.querySelector('span:nth-child(1) > a:nth-child(2)').textContent.trim();
         let anchor = document.createElement("a");
         anchor.setAttribute("href", "#");
+        anchor.setAttribute("title", "Blacklist le topic");
         anchor.onclick = function () { addTopicIdBlacklist(topicId, topicSubject, true); refreshTopicIdKeys(); };
         anchor.innerHTML = '<svg viewBox="2 2 160 160" width="13"><use href="#forbiddenlogo"/></svg>';
         span.appendChild(anchor)
