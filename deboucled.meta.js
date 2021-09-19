512c10730a7611a164d1f309646be500f7bf6baa
Rand0max <rand0max@protonmail.com>
2021-09-19 17:09:33 +0200

    Add meta.js

---
+++ deboucled.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Déboucled
+// @namespace   deboucledjvcom
+// @version     1.1.1
+// @downloadURL https://github.com/Rand0max/deboucled/raw/master/deboucled.user.js
+// @updateURL   https://github.com/Rand0max/deboucled/raw/master/deboucled.meta.js
+// @author      Rand0max
+// @description Censure les topics eclatax et vous sort de la boucle
+// @include     http://www.jeuxvideo.com/forums/*
+// @include     https://www.jeuxvideo.com/forums/*
+// @include     http://m.jeuxvideo.com/forums/*
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_addStyle
+// @grant       GM_deleteValue
+// @grant       GM_listValues
+// ==/UserScript==
--- deboucled.user.js
+++ deboucled.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name        Déboucled
 // @namespace   deboucledjvcom
+// @version     1.1.1
+// @downloadURL https://github.com/Rand0max/deboucled/raw/master/deboucled.user.js
+// @updateURL   https://github.com/Rand0max/deboucled/raw/master/deboucled.meta.js
+// @author      Rand0max
 // @description Censure les topics eclatax et vous sort de la boucle
 // @include     http://www.jeuxvideo.com/forums/*
 // @include     https://www.jeuxvideo.com/forums/*
 // @include     http://m.jeuxvideo.com/forums/*
-// @version     1.1.0
-// @author      Rand0max
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_addStyle
