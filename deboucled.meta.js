a35b4b455c94bdc6e2c19b5d4cdeb4d66fa3ff58
Rand0max <rand0max@protonmail.com>
2021-10-12 15:23:30 +0200

    Replace homepage button by 'back to forum' on error page

--- deboucled.meta.js
+++ deboucled.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Déboucled
 // @namespace   deboucledjvcom
-// @version     1.11.3
+// @version     1.11.4
 // @downloadURL https://github.com/Rand0max/deboucled/raw/master/deboucled.user.js
 // @updateURL   https://github.com/Rand0max/deboucled/raw/master/deboucled.meta.js
 // @author      Rand0max
