034b06afa5d8dbe9a24850cafbd667542740e061
Rand0max <rand0max@protonmail.com>
2021-10-26 15:49:36 +0200

    Escape regex

--- deboucled.user.js
+++ deboucled.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Déboucled
 // @namespace   deboucledjvcom
-// @version     1.16.7
+// @version     1.16.8
 // @downloadURL https://github.com/Rand0max/deboucled/raw/master/deboucled.user.js
 // @updateURL   https://github.com/Rand0max/deboucled/raw/master/deboucled.meta.js
 // @author      Rand0max
@@ -51,7 +51,7 @@ let sortModeSubject = 0;
 let sortModeAuthor = 0;
 let sortModeTopicId = 0;
 
-const deboucledVersion = '1.16.7'
+const deboucledVersion = '1.16.8'
 const topicByPage = 25;
 
 const entitySubject = 'subject';
@@ -266,8 +266,8 @@ function normalizeValue(value) {
 
 function makeRegex(array, withBoundaries) {
     // \b ne fonctionne pas avec les caractères spéciaux
-    let b1 = withBoundaries ? '(?<=\W|^)' : '';
-    let b2 = withBoundaries ? '(?=\W|$)' : '';
+    let b1 = withBoundaries ? '(?<=\\W|^)' : '';
+    let b2 = withBoundaries ? '(?=\\W|$)' : '';
     let map = array.map((e) => {
         let word = e.escapeRegexPattern().handleGenericChar().normalizeDiacritic();
         return `${b1}${word}${b2}`;
