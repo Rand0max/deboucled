c281092e4bade628b7a2ff3d5773704c21873231
Rand0max <rand0max@protonmail.com>
2021-11-02 22:59:18 +0100

    Adapt topic overlay to light/dark theme

--- deboucled.user.js
+++ deboucled.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Déboucled
 // @namespace   deboucledjvcom
-// @version     1.19.2
+// @version     1.19.3
 // @downloadURL https://github.com/Rand0max/deboucled/raw/master/deboucled.user.js
 // @updateURL   https://github.com/Rand0max/deboucled/raw/master/deboucled.meta.js
 // @author      Rand0max
@@ -52,7 +52,7 @@ let sortModeSubject = 0;
 let sortModeAuthor = 0;
 let sortModeTopicId = 0;
 
-const deboucledVersion = '1.19.2'
+const deboucledVersion = '1.19.3'
 const topicByPage = 25;
 
 const entitySubject = 'subject';
