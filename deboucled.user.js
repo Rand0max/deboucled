ff5d081cd3700b9786cdd0c2daff1196916d55ae
Rand0max <rand0max@protonmail.com>
2021-12-10 20:22:36 +0100

    Version

--- deboucled.user.js
+++ deboucled.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Déboucled
 // @namespace   deboucledjvcom
-// @version     2.0.1
+// @version     2.0.3
 // @downloadURL https://github.com/Rand0max/deboucled/raw/master/deboucled.user.js
 // @updateURL   https://github.com/Rand0max/deboucled/raw/master/deboucled.meta.js
 // @author      Rand0max
