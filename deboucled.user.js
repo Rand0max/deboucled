4a21e1b1c17914ab848b67362d75444c400bf596
Rand0max <rand0max@protonmail.com>
2022-01-28 12:41:00 +0100

    Update PeepoJVC theme

--- deboucled.user.js
+++ deboucled.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Déboucled
 // @namespace   deboucledjvcom
-// @version     2.14.4
+// @version     2.14.5
 // @downloadURL https://github.com/Rand0max/deboucled/raw/master/deboucled.user.js
 // @updateURL   https://github.com/Rand0max/deboucled/raw/master/deboucled.meta.js
 // @author      Rand0max
