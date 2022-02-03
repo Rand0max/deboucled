da751995d7a59f4ad64eeb8a6e87b75271e372a2
Rand0max <rand0max@protonmail.com>
2022-02-03 08:17:57 +0100

    Fix theme icons on mobile

--- deboucled.user.js
+++ deboucled.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Déboucled
 // @namespace   deboucledjvcom
-// @version     2.15.4
+// @version     2.15.5
 // @downloadURL https://github.com/Rand0max/deboucled/raw/master/deboucled.user.js
 // @updateURL   https://github.com/Rand0max/deboucled/raw/master/deboucled.meta.js
 // @author      Rand0max
--- variables.js
+++ variables.js
@@ -3,7 +3,7 @@
 // VARIABLES
 ///////////////////////////////////////////////////////////////////////////////////////
 
-const deboucledVersion = '2.15.4'
+const deboucledVersion = '2.15.5'
 const defaultTopicCount = 25;
 
 const entitySubject = 'subject';
