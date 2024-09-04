372bb6faa8a4e89a5d415ccaeac594609647e3db
Rand0max <rand0max@protonmail.com>
2024-09-05 00:07:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -116,6 +116,11 @@ function addSvgs() {
     addSvg(smoothScrollSvg);
 }
 
+function enableMobileZoom() {
+    const viewportMeta = document.querySelector('#meta-viewport');
+    if (viewportMeta) viewportMeta.setAttribute('content', 'width=device-width, initial-scale=1.0');
+}
+
 function getCurrentPageType(url) {
     if (document.querySelector('.img-erreur') !== null) return 'error';
 
@@ -979,7 +984,7 @@ async function entryPoint() {
 
         updateUser();
         suggestUpdate();
-        allowMobileZoom();
+        enableMobileZoom();
 
         displaySecret();
         displayAnnouncement();
