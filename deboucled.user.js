75466629e072e61c4f2a2bd832029a7ec3f69ea1
Rand0max <rand0max@protonmail.com>
2026-04-19 15:05:07 +0200

    Fix css

--- deboucled.user.js
+++ deboucled.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Déboucled
 // @namespace   deboucledjvcom
-// @version     2.80.0
+// @version     2.80.1
 // @downloadURL https://github.com/Rand0max/deboucled/raw/master/deboucled.user.js
 // @updateURL   https://github.com/Rand0max/deboucled/raw/master/deboucled.meta.js
 // @author      Rand0max
